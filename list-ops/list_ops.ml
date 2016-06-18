41a19465071b57567e9ab57b837cdb41baa8209b
Steve <stevejb71@gmail.com>
2016-06-18 16:37:51 +0800

    list-ops

---
+++ list-ops/list_ops.ml
@@ -0,0 +1,13 @@
+let length xs = 0
+
+let reverse xs = xs
+
+let map ~f xs = failwith "undefined"
+
+let filter ~f xs = xs
+
+let fold ~init ~f xs = failwith "undefined"
+
+let append xs ys = xs
+
+let concat xss = failwith "undefined"
