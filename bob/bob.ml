35f5bd0c253b55da478ac6e7a75c89350ed9673f
Steve <stevejb71@gmail.com>
2016-06-18 16:42:53 +0800

    Make Bob more point-free.

--- bob/bob.ml
+++ bob/bob.ml
@@ -1,19 +1,22 @@
 open Core.Std
 
-let is_shouting str =
-  String.for_all ~f:(fun c -> Char.is_lowercase c |> not) str
+let neg p x = p x |> not
+
+let is_shouting =
+  String.for_all ~f:(neg Char.is_lowercase)
 
 let is_question str = str.[String.length str - 1] = '?'
 
 let is_silence str = String.strip str |> String.is_empty
 
-let is_only_nonalpha str =
-  String.for_all ~f:(fun c -> (Char.is_alpha c |> not)) str
+let is_only_nonalpha =
+  String.for_all ~f:(neg Char.is_alpha)
 
 let response_for speech =
   if is_silence speech then "Fine. Be that way!"
   else if is_question speech then
-    if String.drop_suffix speech 1 |> String.for_all ~f:(fun ch -> Char.is_uppercase ch || ch = ' ')
+    if String.drop_suffix speech 1
+       |> String.for_all ~f:(fun ch -> Char.is_uppercase ch || ch = ' ')
     then "Whoa, chill out!"
     else "Sure."
   else if is_only_nonalpha speech then "Whatever."
