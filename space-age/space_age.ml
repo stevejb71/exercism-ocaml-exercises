b21a82454a158ae5959e82768d642390548ccf29
Steve <stevejb71@gmail.com>
2016-07-02 15:56:18 +0800

    space-age

--- space-age/space_age.ml
+++ space-age/space_age.ml
@@ -5,7 +5,7 @@ let oneEarthYearInSeconds = 31557600.0
 
 let toEarthYears (factor: float) (secs: int) = (float_of_int secs /. ((factor *. oneEarthYearInSeconds)))
 
-let age_on p secs = toEarthYears (match p with
+let age_on p = toEarthYears(match p with
   | Mercury -> 0.2408467
   | Venus -> 0.61519726
   | Earth -> 1.0
@@ -13,4 +13,4 @@ let age_on p secs = toEarthYears (match p with
   | Jupiter -> 11.862615
   | Saturn -> 29.447498
   | Uranus -> 84.016846
-  | Neptune -> 164.79132) secs
+  | Neptune -> 164.79132)
