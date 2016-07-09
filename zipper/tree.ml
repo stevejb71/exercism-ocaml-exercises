1aeb64f2fe1e8247255174cedc104f7661c0b78e
Steve <stevejb71@gmail.com>
2016-07-09 10:38:32 +0800

    zipper

---
+++ zipper/tree.ml
@@ -0,0 +1,8 @@
+open Core.Std
+open Sexplib.Conv
+
+type 'a t = { value : 'a; left : 'a t option; right : 'a t option } [@@deriving sexp]
+let rec equal veq a b =
+  veq a.value b.value &&
+  Option.equal (equal veq) a.left b.left &&
+  Option.equal (equal veq) a.right b.right
