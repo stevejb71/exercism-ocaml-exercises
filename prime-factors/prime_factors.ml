0faaf07ad23caea0ef386650ab77bc8ec5fb05da
Steve <stevejb71@gmail.com>
2016-07-11 11:54:00 +0800

    prime_factors using Core Sequence

--- prime-factors/prime_factors.ml
+++ prime-factors/prime_factors.ml
@@ -1,29 +1,28 @@
-open Batteries
 open Core.Std
 open Int64
-module BL = Batteries.List
+module S = Sequence
 
-let isqrt (n: int64): int64 = n |! to_float |> sqrt |> of_float
+let isqrt (n: int64): int64 = n |! Big_int.big_int_of_int64 |> Big_int.sqrt_big_int |> Big_int.int64_of_big_int
 
-let possible_divisors (n: int64): int64 BatEnum.t =
-  let max_divisor = isqrt n
-  in BL.enum [
-    Enum.singleton 2L;
-    Enum.singleton 3L;
-    Enum.seq 5L ((+) 6L) ((>=) max_divisor);
-    Enum.seq 7L ((+) 6L) ((>=) max_divisor);
-    Enum.singleton n;
-    ] |> Enum.flatten
+(** Returns a sequence of the form [a; f a; f f a;...]. *)
+let from (start: 'a) (f: 'a -> 'a): 'a Sequence.t =
+  S.unfold ~init:start ~f:(fun s -> Some (s, f s))
 
-let find_first_divisor (n: int64): int64 =
-  Enum.find (fun f -> rem n f = 0L) (possible_divisors n)
+let possible_proper_divisors (n: int64): int64 Sequence.t =
+  let max_divisor = isqrt n in
+  S.of_list [
+    S.of_list [2L; 3L];
+    from 5L ((+) 6L);
+    from 7L ((+) 6L);
+  ] |> S.concat |> S.take_while ~f:((>=) max_divisor)
+
+let first_proper_divisor (n: int64): int64 option =
+  possible_proper_divisors n |> S.find ~f:(fun f -> n % f = 0L)
 
 let rec find_factors_of (n: int64): int64 list =
-  let first_divisor = find_first_divisor n
-  in
-    if first_divisor = n
-    then [n]
-    else first_divisor :: (find_factors_of (n / first_divisor))
+  match first_proper_divisor n with
+    | None -> [n]
+    | Some d -> d :: (find_factors_of (n / d))
 
 let factors_of = function
   | 1L -> []
