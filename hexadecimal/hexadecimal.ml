d810a0d52b343622eda250966215552e1022d0af
Steve <stevejb71@gmail.com>
2016-07-11 11:27:51 +0800

    hexadecimal update

--- hexadecimal/hexadecimal.ml
+++ hexadecimal/hexadecimal.ml
@@ -1,14 +1,8 @@
 open Core.Std
 
-let letter_to_hex ch = match ch with
-  | '0'..'9' -> Some (Char.to_int ch - 48)
-  | 'a'..'f' -> Some (Char.to_int ch - 87)
-  | 'A'..'F' -> Some (Char.to_int ch - 55)
-  | _ -> None
-
-let convert_digit acc ch =
-  Option.map2 acc (letter_to_hex ch) (fun acc d -> acc * 16 + d)
-
-let to_int s =
-  let maybe_hex = String.fold ~init:(Some 0) ~f:convert_digit s in
-  Option.value ~default:0 maybe_hex
+let to_int =
+  let to_hex ch r = match Char.uppercase ch with
+    | '0'..'9' -> Char.to_int ch - Char.to_int '0'
+    | 'A'..'F' -> Char.to_int ch - Char.to_int 'a' + 10
+    | _ -> r.return 0 in
+  String.fold ~init:0 ~f:(fun acc ch -> with_return (fun r -> acc * 16 + (to_hex ch r)))
