8508f84cb0fd4a8908995b2d3b5cbd0cf7ddbb53
Steve <stevejb71@gmail.com>
2016-07-17 16:07:07 +0800

    custom_set

--- custom-set/Custom_set.ml
+++ custom-set/Custom_set.ml
@@ -7,72 +7,76 @@ module type ELEMENT = sig
   val to_string : t -> string
 end
 
+type 'a node = {
+  value: 'a;
+  left: 'a bst;
+  right: 'a bst;
+}
+and 'a bst = Empty | Node of 'a node
+
+let singleton x = Node {value = x; left = Empty; right = Empty}
+
+let (<<) f g x = f (g x)
+
 module Make(El: ELEMENT) = struct
-  (* Not terribly efficient, but good enough for this example.
-   *
-   * The list is kept sorted at all times. *)
-  type t = El.t list
-
   type el = El.t
+  type t = el bst
 
-  let is_empty = List.is_empty
+  let (<) (l: el) (r: el): bool = El.compare l r < 0
+  let (=) = El.equal
 
-  let rec is_member l n = List.find l ~f:(El.equal n) |> Option.is_some
+  let empty = Empty
 
-  let equal a b = List.equal a b ~equal:El.equal
+  let is_empty = function
+    | Empty -> true
+    | _ -> false
 
-  let to_string l =
-    let rec print_els = function
-      | (h1::h2::t) -> El.to_string h1 ^ " " ^ print_els (h2::t)
-      | (h1::t) -> El.to_string h1 ^ print_els t
-      | [] -> "" in
-    "{" ^ print_els l ^ "}"
+  let rec is_member x n = match x with
+    | Node {value = v; _} when v = n -> true
+    | Node {left = l; value = v; _} when n < v -> is_member l n
+    | Node {right = r; _} -> is_member r n
+    | _ -> false
 
-  let empty = []
+  let rec equal a b = match (a, b) with
+    | (Empty, Empty) -> true
+    | (Node {left = l1; value = v1; right = r1}, Node {left = l2; value = v2; right = r2}) ->
+        v1 = v2 && equal l1 l2 && equal r1 r2
+    | _ -> false
 
-  let of_list l = List.dedup ~compare:El.compare l |> List.sort ~cmp:El.compare
+  let rec add x n = match x with
+    | Empty -> singleton n
+    | Node {value = v; _} when n = v -> x
+    | Node {left = l; value = v; right = r} when n < v ->
+        Node {left = add l n; right = r; value = v}
+    | Node {left = l; value = v; right = r} ->
+        Node {left = l; right = add r n; value = v}
 
-  let to_list l = l
+  let rec to_list = function
+    | Empty -> []
+    | Node {value = v; left = l; right = r} -> to_list l @ (v :: to_list r)
 
-  let add l x =
-    let rec go acc = function
-      | h :: t ->
-        let r = El.compare x h in
-        if r = 0 then l (* element already in the set *)
-        else if r < 0 then List.rev_append acc (x :: h :: t)
-        else go (h::acc) t
-      | [] -> List.rev (x::acc)
-    in go [] l
+  let of_list_from init = List.fold ~init ~f:add
 
-  let remove l x =
-    let rec go acc = function
-      | h :: t ->
-        let r = El.compare x h in
-        if r = 0 then List.rev_append acc t
-        else if r < 0 then l
-        else go (h::acc) t
-      | [] -> l
-    in go [] l
+  let union x = of_list_from x << to_list
 
-  type status = [
-    | `OnlyA
-    | `OnlyB
-    | `Both
-  ]
+  let of_list = of_list_from empty
 
-  let diff_filter (f : status -> bool) l1 l2 =
-    let rec go acc = function
-      | [], [] -> List.rev acc
-      | (h1::t1), [] -> go (if f `OnlyA then h1::acc else acc) (t1, [])
-      | [], (h2::t2) -> go (if f `OnlyB then h2::acc else acc) ([], t2)
-      | (h1::t1), (h2::t2) ->
-        let r = El.compare h1 h2 in
-        if r == 0 then go (if f `Both then h1::acc else acc) (t1, t2)
-        else if r < 0 then go (if f `OnlyA then h1::acc else acc) (t1, h2::t2)
-        else go (if f `OnlyB then h2::acc else acc) (h1::t1, t2)
-    in go [] (l1, l2)
+  let to_string x =
+    let l = List.map ~f:El.to_string (to_list x) in
+    sprintf "{%s}" (String.concat ~sep:" " l)
 
-  let difference = diff_filter (function `OnlyA -> true | _ -> false)
-  let intersect = diff_filter (function `Both -> true | _ -> false)
-  let union = diff_filter (fun _ -> true)
+  let rec remove x n = match x with
+    | Empty -> Empty
+    | Node {left = l; value = v; right = r} when n = v -> (
+        match (l, r) with
+          | (Empty, _) -> r
+          | (_, Empty) -> l
+          | (Node {left = ll; value = vl; right = rl}, _) -> Node {left = remove ll n; value = vl; right = rl}
+        )
+    | Node {left = l; value = v; right = r} when n < v -> Node {left = remove l n; value = v; right = r}
+    | Node {left = l; value = v; right = r} -> Node {left = l; value = v; right = remove r n}
+
+  let difference x = List.fold ~init:x ~f:remove << to_list
+
+  let intersect xs = List.fold ~init:empty ~f:(fun acc y -> if is_member xs y then add acc y else acc) << to_list
 end
--- prime-factors/prime_factors.ml
+++ prime-factors/prime_factors.ml
@@ -5,16 +5,15 @@ module S = Sequence
 let isqrt (n: int64): int64 = n |! Big_int.big_int_of_int64 |> Big_int.sqrt_big_int |> Big_int.int64_of_big_int
 
 (** Returns a sequence of the form [a; f a; f f a;...]. *)
-let from (start: 'a) (f: 'a -> 'a): 'a Sequence.t =
-  S.unfold ~init:start ~f:(fun s -> Some (s, f s))
+let repeatedly (init: 'a) (f: 'a -> 'a): 'a Sequence.t =
+  S.unfold ~init ~f:(fun s -> Some (s, f s))
 
 let possible_proper_divisors (n: int64): int64 Sequence.t =
   let max_divisor = isqrt n in
   S.of_list [
     S.of_list [2L; 3L];
-    from 5L ((+) 6L);
-    from 7L ((+) 6L);
-  ] |> S.concat |> S.take_while ~f:((>=) max_divisor)
+    repeatedly 1L ((+) 1L) |> S.concat_map ~f:(fun x -> S.of_list [6L*x - 1L; 6L*x + 1L]);
+  ] |> S.concat |> S.take_while ~f:(fun n -> n <= max_divisor)
 
 let first_proper_divisor (n: int64): int64 option =
   possible_proper_divisors n |> S.find ~f:(fun f -> n % f = 0L)
