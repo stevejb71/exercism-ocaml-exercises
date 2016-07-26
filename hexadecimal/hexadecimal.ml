bda48ff056d4a3dc77ffe4bc2cecb77d5528babe
Steve <stevejb71@gmail.com>
2016-07-26 12:52:44 +0800

    hexadecimal fix

--- hexadecimal/hexadecimal.ml
+++ hexadecimal/hexadecimal.ml
@@ -1,8 +1,10 @@
 open Core.Std
 
-let to_int =
-  let to_hex ch r = match Char.uppercase ch with
-    | '0'..'9' -> Char.to_int ch - Char.to_int '0'
-    | 'A'..'F' -> Char.to_int ch - Char.to_int 'a' + 10
-    | _ -> r.return 0 in
-  String.fold ~init:0 ~f:(fun acc ch -> with_return (fun r -> acc * 16 + (to_hex ch r)))
+let to_int s =
+  with_return(fun r ->
+    let to_hex ch = let ch = Char.lowercase ch in match ch with
+      | '0'..'9' -> Char.to_int ch - Char.to_int '0'
+      | 'a'..'f' -> Char.to_int ch - Char.to_int 'a' + 10
+      | _ -> r.return 0 in
+    String.fold ~init:0 ~f:(fun acc ch -> acc * 16 + to_hex ch) s
+  )
